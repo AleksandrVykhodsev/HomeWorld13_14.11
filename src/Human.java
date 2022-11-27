e58b0f14a6ee9825cab4a4a510a0eef866c10fcd
Alex <89831127161@mail.ru>
2022-11-27 23:15:33 +0300

    Сделано ДЗ № 2

--- src/Human.java
+++ src/Human.java
@@ -1,7 +1,7 @@
 public class Human {
     String name;
-    int yearOfBirth;
-    String town;
+    private int yearOfBirth;
+    private String town;
     String jobTitle;
 
 
@@ -11,16 +11,9 @@ public class Human {
         }else {
             this.name = name;
         }
-        if (yearOfBirth < 0){
-            this.yearOfBirth = 0;
-        }else {
-            this.yearOfBirth = yearOfBirth;
-        }
-        if (town == null){
-            this.town = "Информация не указана";
-        }else {
-            this.town = town;
-        }
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+
         if (jobTitle == null){
             this.jobTitle = "Информация не указана";
         }else {
@@ -32,5 +25,30 @@ public class Human {
     @Override
     public String toString() {
         return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth < 0 || yearOfBirth == 0){
+            this.yearOfBirth = 0;
+        }else {
+            this.yearOfBirth = yearOfBirth;
+        }
+
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown() {
+        if (town == null || !town.isEmpty() || !town.isBlank()){
+            this.town = "Информация не указана";
+        }else {
+            this.town = town;
+        }
     }
 }
